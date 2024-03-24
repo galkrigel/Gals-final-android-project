40a633d6516878ba154b24c6cedc9f489ecfd08c
galkrigel <galkrigel@gmail.com>
2024-03-24 19:16:34 +0200

    add navigation from user properties

--- Final-project-android/app/src/main/java/com/example/final_project_android/MainActivity.kt
+++ Final-project-android/app/src/main/java/com/example/final_project_android/MainActivity.kt
@@ -42,7 +42,7 @@ class MainActivity : AppCompatActivity() {
         val navHostFragment: NavHostFragment? =
             supportFragmentManager.findFragmentById(R.id.navHostMain) as? NavHostFragment
         navController = navHostFragment?.navController
-        navController?.let { NavigationUI.setupActionBarWithNavController(this, it) }
+        // navController?.let { NavigationUI.setupActionBarWithNavController(this, it) }
 
         val bottomNavigationView: BottomNavigationView =
             findViewById(R.id.mainActivityBottomNavigationView)
@@ -64,11 +64,11 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
-        super.onCreateOptionsMenu(menu)
-        menuInflater.inflate(R.menu.menu, menu)
-        return true
-    }
+//    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+//        super.onCreateOptionsMenu(menu)
+//        menuInflater.inflate(R.menu.menu, menu)
+//        return true
+//    }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         return when (item.itemId) {
@@ -83,61 +83,3 @@ class MainActivity : AppCompatActivity() {
         }
     }
 }
-
-
-//package com.example.final_project_android
-//import retrofit2.Call
-//import retrofit2.Callback
-//import retrofit2.Response
-//import android.content.Intent
-//import androidx.appcompat.app.AppCompatActivity
-//import android.os.Bundle
-//import android.view.Menu
-//import android.view.MenuItem
-//import androidx.navigation.NavController
-//import androidx.navigation.fragment.NavHostFragment
-//import androidx.navigation.ui.NavigationUI
-//import com.google.android.material.bottomnavigation.BottomNavigationView
-//
-//
-//
-//class MainActivity : AppCompatActivity() {
-//    var navController: NavController? = null
-//
-//    override fun onCreate(savedInstanceState: Bundle?) {
-//        super.onCreate(savedInstanceState)
-//        setContentView(R.layout.activity_main)
-//
-//        val navHostFragment: NavHostFragment? =
-//            supportFragmentManager.findFragmentById(R.id.navHostMain) as? NavHostFragment
-//        navController = navHostFragment?.navController
-//        navController?.let { NavigationUI.setupActionBarWithNavController(this, it) }
-//
-//        val bottomNavigationView: BottomNavigationView =
-//            findViewById(R.id.mainActivityBottomNavigationView)
-//        navController?.let { NavigationUI.setupWithNavController(bottomNavigationView, it) }
-//    }
-//
-//
-//
-//
-//
-//    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
-//        super.onCreateOptionsMenu(menu)
-//        menuInflater.inflate(R.menu.menu, menu)
-//        return true
-//    }
-//
-//    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-//        return when (item.itemId) {
-//            android.R.id.home -> {
-//                navController?.navigateUp()
-//                true
-//            }
-//
-//            else -> navController?.let { NavigationUI.onNavDestinationSelected(item, it) }
-//                ?: super.onOptionsItemSelected(item)
-//
-//        }
-//    }
-//}
